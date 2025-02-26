9070e7e7a56138de1a5de8412e610cfd61265d13
github 200BingSu <github 200BingSu@gmail.com>
2025-02-26 11:12:07 +0900

    apk

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
